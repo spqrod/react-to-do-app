0759414df93a2c4b3dc4f09a82359ec4804e6049
rodionion <rodionvh@gmail.com>
2023-03-10 13:37:17 -0300

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Overview from "./components/overview";
+import uniqid from "uniqid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      task: {
+        text: "",
+        id: uniqid()
+      },
+      tasks: [],
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      task : {
+        text: e.target.value,
+        id: this.state.task.id
+      }
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      task: {
+        text: "",
+        id: uniqid()
+      },
+      tasks: this.state.tasks.concat(this.state.task)
+    });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="taskInput">Add your task</label>
+          <input type="text" placeholder="Write something.." name="taskInput" onChange={this.handleInputChange} value={task.text}></input>
+          <button type="submit">
+            Submit Task
+          </button>
+        </form>
+        <Overview tasks={tasks}/>
+      </div>
+    );
+  }
 }
 
 export default App;
